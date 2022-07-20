bcb3e7f341e8c43b4b540c4d55d05b74cf69076e
Nikunj <nikunjgoyal31@gmail.com>
2022-07-20 19:15:26 +0530

    added the main jumbotron and some animations as well :)

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSub from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTelegram } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -22,6 +23,31 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/nikunjgoyal31/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Nike682631"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/nikunjgoyal31" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon icon={faTelegram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
 )
 
